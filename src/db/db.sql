43787ed283923ad31805d942fa6829cf5a9b968a
Ode42 <ode.progr@outlook.com>
2022-01-23 20:37:56 +0200

    Database tables for resources and offices

--- src/db/db.sql
+++ src/db/db.sql
@@ -8,3 +8,16 @@ CREATE TABLE projects(
     project_description VARCHAR(500),
     project_due DATE NOT NULL
 );
+
+CREATE TABLE resources(
+    resource_id SERIAL PRIMARY KEY,
+    resource_name VARCHAR(255),
+    resource_description VARCHAR(255),
+    resources_amount NUMERIC
+);
+
+CREATE TABLE offices(
+    office_id SERIAL PRIMARY KEY,
+    offce_name VARCHAR(255),
+    office_place VARCHAR(255)
+);
