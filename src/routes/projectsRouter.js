c278c55d6c49582d5916a7d1aa576a788d4d2ed5
Ode42 <ode.progr@outlook.com>
2022-02-23 12:29:11 +0200

    Single project endpoint

--- src/routes/projectsRouter.js
+++ src/routes/projectsRouter.js
@@ -1,23 +1,34 @@
 const express = require("express");
 const Router = require("express").Router;
-const pool = require("./../db/db")
+const pool = require("./../db/db");
 const projectsRouter = Router();
 
 projectsRouter.get("/", async (request, response) => {
-    try {
-        response.json("message: Moiz");
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    response.json("message: 'Moiz'");
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-
 projectsRouter.get("/all", async (request, response) => {
-    try {
-        const projects = await pool.query("SELECT * FROM projects;");
-        response.json(projects);
-    } catch (error) {
-        console.error(error);
-    }
-})
+  try {
+    const projects = await pool.query("SELECT * FROM projects;");
+    response.json(projects);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+projectsRouter.get("/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const project = await pool.query(
+      `SELECT * FROM projects WHERE project_id = ${id}`
+    );
+    response.json(project.rows[0]);
+  } catch (error) {
+    console.error(error);
+  }
+});
 module.exports = projectsRouter;
