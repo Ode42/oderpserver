a8f1d0dff8e6d287584a67bd69f65174d87afe47
Ode42 <ode.progr@outlook.com>
2022-02-23 12:18:45 +0200

    SQL types correctly

--- src/db/db.sql
+++ src/db/db.sql
@@ -3,21 +3,21 @@ CREATE DATABASE oderp;
 -- \c into oderp
 
 CREATE TABLE projects(
-    project_id SERIAL PRIMARY KEY,
-    project_name VARCHAR(255),
+    project_id BIGSERIAL NOT NULL PRIMARY KEY,
+    project_name VARCHAR(50) NOT NULL,
     project_description VARCHAR(500),
     project_due DATE NOT NULL
 );
 
 CREATE TABLE resources(
-    resource_id SERIAL PRIMARY KEY,
-    resource_name VARCHAR(255),
+    resource_id BIGSERIAL NOT NULL PRIMARY KEY,
+    resource_name VARCHAR(50) NOT NULL,
     resource_description VARCHAR(255),
     resources_amount NUMERIC
 );
 
 CREATE TABLE offices(
-    office_id SERIAL PRIMARY KEY,
-    offce_name VARCHAR(255),
-    office_place VARCHAR(255)
+    office_id BIGSERIAL NOT NULL PRIMARY KEY,
+    offce_name VARCHAR(50) NOT NULL,
+    office_place VARCHAR(255) NOT NULL
 );
