df80c9bfa90487ffb9fb21df95522285590a2993
Ode42 <ode.progr@outlook.com>
2022-03-05 11:04:05 +0200

    Email column unique

--- src/db/db.sql
+++ src/db/db.sql
@@ -3,11 +3,11 @@ CREATE DATABASE oderp;
 -- \c into oderp
 
 CREATE TABLE users(
-    user_id BIGSERIAL NOT NULL PRIMARY KEY,
+    user_id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(75) NOT NULL,
     last_name VARCHAR(75) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) UNIQUE NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE projects(
